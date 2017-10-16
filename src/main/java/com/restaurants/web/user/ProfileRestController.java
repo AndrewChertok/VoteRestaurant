d028a1b65506cf8dfa5e38d32bbf1e85069b58f9
Andrey <fyling@i.ua>
2017-10-16 23:22:06 +0300

    rest controller

--- src/main/java/com/restaurants/web/user/ProfileRestController.java
+++ src/main/java/com/restaurants/web/user/ProfileRestController.java
@@ -2,19 +2,27 @@ package com.restaurants.web.user;
 
 import com.restaurants.AuthorizedUser;
 import com.restaurants.model.User;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
-
+@RestController
+@RequestMapping(value = ProfileRestController.URL)
 public class ProfileRestController extends AbstractUserController{
 
+    static final String URL = "/user";
+
+    @DeleteMapping(value = "/delete")
     public void delete() {
         super.delete(AuthorizedUser.id());
     }
-    
+
+    @GetMapping(value = "/get", produces = MediaType.APPLICATION_JSON_VALUE)
     public User get() {
         return super.get(AuthorizedUser.id());
     }
-    
-    public void update(User user) {
+
+    @PutMapping(value = "/update", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public void update(@RequestBody User user) {
         super.update(user, AuthorizedUser.id());
     }
 
