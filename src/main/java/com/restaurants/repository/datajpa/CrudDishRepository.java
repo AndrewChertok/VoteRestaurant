a989d0c4cc3db4e764190c50632db3c70faa25b2
Andrey <fyling@i.ua>
2017-10-16 23:24:06 +0300

    rest controller

--- src/main/java/com/restaurants/repository/datajpa/CrudDishRepository.java
+++ src/main/java/com/restaurants/repository/datajpa/CrudDishRepository.java
@@ -5,16 +5,35 @@ import com.restaurants.model.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Transactional(readOnly = true)
 public interface CrudDishRepository extends JpaRepository<Dish, Integer>{
 
+    @Transactional
     @Override
     Dish save(Dish dish);
 
+    @Transactional
     @Override
     void deleteById(Integer id);
 
+    @Override
+    Optional<Dish> findById(Integer id);
+
+    @Transactional
     @Modifying
     @Query("UPDATE Dish d SET d.restaurant = ?1 WHERE d.id = ?2")
     void setRestaurant(Restaurant restaurant, Integer id);
+
+    @Query("SELECT d FROM Dish d order by d.createdOrUpdated DESC ")
+    List<Dish> findAll();
+
+    @Query("SELECT d FROM Dish d WHERE d.createdOrUpdated BETWEEN :startDate AND :endDate ORDER BY d.createdOrUpdated DESC ")
+    List<Dish> getBetweenDates(@Param("startDate")LocalDate startDate, @Param("endDate")LocalDate endDate);
 }
